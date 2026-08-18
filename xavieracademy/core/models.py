f176fb08fba760908c2679d3a61b1053f2ef2a2e
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2026-08-18 09:51:17 -0300

    adição do atributo cep na classe pessoa

--- xavieracademy/core/models.py
+++ xavieracademy/core/models.py
@@ -26,6 +26,7 @@ class Pessoa(models.Model):
     bairro = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
     estado = models.CharField(max_length=2)
+    cep = models.CharField(max_length=9)
 
     STATUS_CHOICES = [
         ('ATIVO', 'Ativo'),
