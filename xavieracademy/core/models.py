0523f2c408798a6d2b195b1e8f55ff166b17f149
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2026-08-18 12:22:22 -0300

    correção de abstract user para user

--- xavieracademy/core/models.py
+++ xavieracademy/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-class Usuario(AbstractUser):
+class Usuario(User):
     def __str__(self):
         return self.username
 
